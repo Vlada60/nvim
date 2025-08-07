8df6e66a0bc1afb27f592613781e259bc04057dd
Vladimir Đorđević <vlada.dj@protonmail.com>
2025-08-08 01:37:30 +0200

    html and css lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -221,6 +221,8 @@ return {
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
+        html = {},
+        cssls = {},
 
         prettier = {},
         prettierd = {},
